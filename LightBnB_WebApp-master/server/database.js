45aca7d7e934e3e558310b6675dbb47a2093f8f6
Calvin <calvinchan87@gmail.com>
2021-05-03 21:22:09 +0000

    refactor getAllProperties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -132,16 +132,52 @@ const getAllProperties = function(options, limit = 10) {
   // }
   // return Promise.resolve(limitedProperties);
 
-  const queryString = `
+  // const values = [limit];
+  const queryParams = [];
+
+  let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating FROM properties
   JOIN property_reviews ON property_id = properties.id
-  GROUP BY properties.id
-  LIMIT $1;
+  WHERE 1 = 1
   `;
 
-  const values = [limit];
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `AND city LIKE $${queryParams.length} `; // case sensitive!
+  }
 
-  return pool.query(queryString, values)
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `AND owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    queryString += `AND cost_per_night >= $${queryParams.length} `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    queryString += `AND cost_per_night <= $${queryParams.length} `;
+  }
+
+  queryString += `GROUP BY properties.id `
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
+  // return pool.query(queryString, values)
+  return pool.query(queryString, queryParams)
   .then(result => result.rows)
   .catch(err => console.log(err.message));
 };
